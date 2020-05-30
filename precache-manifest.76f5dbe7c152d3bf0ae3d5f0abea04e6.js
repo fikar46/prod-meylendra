e1c372c6b3af59b3a91c40336302f3acab34814c
M Zulfikar maylendra <zulfikar@doku.com>
2020-05-30 13:17:29 +0700

    first commit

---
+++ precache-manifest.76f5dbe7c152d3bf0ae3d5f0abea04e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef5141b706d7032a76c1",
+    "url": "/static/js/main.c700ca6e.chunk.js"
+  },
+  {
+    "revision": "338c4e54017bab6d9fa3",
+    "url": "/static/js/2.db71d58c.chunk.js"
+  },
+  {
+    "revision": "ef5141b706d7032a76c1",
+    "url": "/static/css/main.ceae3152.chunk.css"
+  },
+  {
+    "revision": "338c4e54017bab6d9fa3",
+    "url": "/static/css/2.e2ae87fc.chunk.css"
+  },
+  {
+    "revision": "7c42de1c004551c7454d412f8ee0720d",
+    "url": "/index.html"
+  }
+];
